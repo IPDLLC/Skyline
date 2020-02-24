1301820368b44f17780be9246f0f9bc19ed0921a
Ginlang <shmalliscute@outlook.com>
2020-02-24 17:45:49 +0000

    New status (still needs auto-update feature)

--- modules/status.py
+++ modules/status.py
@@ -2,5 +2,5 @@
 import discord
 
 async def init(self):
-    watching = discord.Activity(name='over {} servers!'.format(len(self.guilds)), type=discord.ActivityType.watching)
+    watching = discord.Activity(name='the behaviour of members on {} guilds'.format(len(self.guilds)), type=discord.ActivityType.watching)
     await self.change_presence(status=discord.Status.dnd, activity=watching)
